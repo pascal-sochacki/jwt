e23cd8e7347d943bbadb92a4cb922d1616a0a0cb
Pascal Sochacki <pascal@sochacki.dev>
2023-12-13 22:36:47 +0100

    read out iat, pipe into cmd

--- cmd/jwt/inspect.go
+++ cmd/jwt/inspect.go
@@ -1,51 +1,67 @@
 package jwt
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
 )
+
+type Body struct {
+	IssuedAt int `json:"iat"`
+}
 
 var InspectCmd = &cobra.Command{
 	Use:     "inspect",
 	Aliases: []string{"ins"},
 	Short:   "inspect a jwt",
-	Args:    cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		jwt := args[0]
+		var jwt string
+		if len(args) == 1 {
+			jwt = args[0]
+		} else {
+			scanner := bufio.NewScanner(os.Stdin)
+			for scanner.Scan() {
+				jwt = scanner.Text()
+			}
+		}
 		parts := strings.Split(jwt, ".")
 
-		println(len(parts))
 		if len(parts) != 3 {
 			fmt.Println("jwt does not contain three parts")
 			os.Exit(1)
 		}
 
-		for _, part := range parts {
-			if l := len(part) % 4; l > 0 {
-				part += strings.Repeat("=", 4-l)
-			}
-
-			decodedData, err := base64.URLEncoding.DecodeString(part)
-			if err != nil {
-				fmt.Printf("could not decode: %s\n", part)
-				os.Exit(1)
-			}
-
-			var pretty bytes.Buffer
-			err = json.Indent(&pretty, []byte(decodedData), "", "  ")
-			if err != nil {
-				fmt.Printf("could not pretty part: %s", part)
-				os.Exit(1)
-			}
-			println(pretty.String())
-
+		if l := len(parts[1]) % 4; l > 0 {
+			parts[1] += strings.Repeat("=", 4-l)
 		}
+
+		decodedData, err := base64.URLEncoding.DecodeString(parts[1])
+		if err != nil {
+			fmt.Printf("could not decode: %s\n", parts[1])
+			os.Exit(1)
+		}
+		var body Body
+		json.Unmarshal(decodedData, &body)
+
+		var pretty bytes.Buffer
+		err = json.Indent(&pretty, []byte(decodedData), "", "  ")
+		if err != nil {
+			fmt.Printf("could not pretty part: %s", parts[1])
+			os.Exit(1)
+		}
+		println(pretty.String())
+
+		iatTime := time.Unix(int64(body.IssuedAt), 0)
+		germanFormat := iatTime.Format("02.01.2006 15:04:05 MST")
+
+		fmt.Printf("Issued At (iat): %s\n", germanFormat)
 
 	},
 }
